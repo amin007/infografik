dd3b1f0cefc5365ef08e345ad1942f92a909de6c
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-05-10 20:20:09 +0800

    tukar lokasi fail

--- sumber/fail/js/highchart/dibawah.php
+++ sumber/fail/js/highchart/dibawah.php
@@ -1,7 +1,7 @@
 <!-- //////////////////////////////////////////////////////////////////////////////////// -->
 <?php
-define('URL', '../../../utama/highchart/7.1.1/code/');
-echo '<hr>url = ' . URL . '<hr>';
+define('URL', '../../../../utama/highchart/7.1.1/code/');
+echo '<hr>url = ' . URL . '<hr>' . "\n";
 ?>
 <script src="<?php echo URL ?>highcharts.js"></script>
 <script src="<?php echo URL ?>modules/data.js"></script>
