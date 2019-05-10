f1763eb3af505836ddfa19f14538eb529b68cd66
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-05-11 00:46:37 +0800

    tambah fail baru

---
+++ sumber/fail/js/highchart/tarikdatacsv/2.1-oil-palm-canopy-biometrics.php
@@ -0,0 +1,58 @@
+<?php
+//include '../diatas.php';
+include '../diatas001.php';
+?>
+
+<!-- x. Add the container -->
+<div class="container">
+	<h1>Oil Plam Canopy Biometrics</h1>
+	<div class="row mb-2">
+	<div class="col-md-6">
+		<div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+			<div class="col p-4 d-flex flex-column position-static">
+				<h3 class="mb-0">Parameters</h3>
+				<div class="mb-1 text-muted">Frond Number</div>
+				<div class="mb-1 text-muted">Frond Angle</div>
+				<div class="mb-1 text-muted">Crown Pixel</div>
+				<div class="mb-1 text-muted">Canopy200</div>
+				<div class="mb-1 text-muted">Canopy800</div>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-6">
+		<div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+			<div class="col p-4 d-flex flex-column position-static">
+				<form>
+				<div class="form-group">
+				<label for="exampleFormControlSelect2">Scanning period:</label>
+				<select class="form-control" id="exampleFormControlSelect2">
+					<option>Frond Number</option>
+					<option>Frond Angle</option>
+					<option>Crown Pixel</option>
+					<option>Canopy200</option>
+					<option>Canopy800</option>
+				</select>
+				</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	</div>
+</div><!-- ./ class="container" -->
+
+
+<!-- FOOTER -->
+<footer class="footer mt-auto py-3">
+<div class="containerFooter">
+	<p class="float-right"><a href="#">Back to top</a></p>
+	<p>&copy; 2017-2019 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+</div><!-- ./ class="container" -->
+</footer>
+
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////// -->
+<!-- script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script -->
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<?php include '../dibawah.php';
+?>
+</body>
+</html>
