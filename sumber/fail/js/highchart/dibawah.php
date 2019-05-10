bf5b8e772995b37e1a929ed99b3b8db2d5e366f8
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-05-10 23:54:24 +0800

    tambah pautan bootstrap

--- sumber/fail/js/highchart/dibawah.php
+++ sumber/fail/js/highchart/dibawah.php
@@ -3,6 +3,7 @@
 define('URL', '../../../../utama/highchart/7.1.1/code/');
 //echo '<hr>url = ' . URL . '<hr>' . "\n";
 ?>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 <script src="<?php echo URL ?>highcharts.js"></script>
 <script src="<?php echo URL ?>modules/data.js"></script>
 <script src="<?php echo URL ?>modules/exporting.js"></script>
