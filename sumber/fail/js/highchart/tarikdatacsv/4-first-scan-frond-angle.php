a067e750c0a68e327c26a7b61fd39c6dda6f9221
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-05-10 22:31:07 +0800

    tambah fail baru

---
+++ sumber/fail/js/highchart/tarikdatacsv/4-first-scan-frond-angle.php
@@ -0,0 +1,44 @@
+<?php include '../diatas.php' ?>
+
+<!-- x. Add the container -->
+<div id="container" style="width: 800px; height: 400px; margin: 0 auto"></div>
+
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////// -->
+<!-- script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script -->
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<?php include '../dibawah.php';
+?>
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////// -->
+<!-- 2. Add the JavaScript to initialize the chart on document ready -->
+<script type="text/javascript">
+$(document).ready(function() {
+
+	$.get('data/first-scan-frond-angle.csv', function(csv)
+	{
+		$('#container').highcharts({
+			chart: { type: 'column'},
+			data: { csv: csv} ,
+			title: {
+				text: 'Frond Angel'
+			},
+			yAxis: {
+				allowDecimals: false,
+				title: { text: '' }
+			},
+			plotOptions: {
+				series: {
+					borderWidth: 0,
+					dataLabels: {
+						enabled: true,
+						format: '{point.y}'
+					}
+				}
+			},
+		});
+	});
+
+});
+</script>
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////// -->
+</body>
+</html>
