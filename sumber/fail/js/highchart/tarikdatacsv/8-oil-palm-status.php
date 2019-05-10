2cd9a4ac72996d2fe9af5b4d6748c25e3b85aa7d
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-05-11 00:37:05 +0800

    tqambah fail baru

---
+++ sumber/fail/js/highchart/tarikdatacsv/8-oil-palm-status.php
@@ -0,0 +1,145 @@
+<?php
+//include '../diatas.php';
+include '../diatas001.php';
+?>
+
+<!-- x. Add the container -->
+<div class="container">
+	<h1>Oil Palm Status</h1>
+	<div class="row mb-2">
+	<div class="col-md-6">
+<!-- //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+<form class="needs-validation" novalidate>
+<div class="mb-3">
+	<label for="username">Frond Number</label>
+	<div class="input-group">
+		<input type="text" class="form-control" id="username" placeholder="***">
+		<div class="input-group-prepend">
+			<span class="input-group-text">Frond Number</span>
+		</div>
+		<div class="invalid-feedback" style="width: 100%;">
+			***
+		</div>
+	</div>
+</div>
+
+<div class="mb-3">
+	<label for="username">Frond Angel</label>
+	<div class="input-group">
+		<input type="text" class="form-control" id="username" placeholder="***">
+		<div class="input-group-prepend">
+			<span class="input-group-text">Frond Angel</span>
+		</div>
+		<div class="invalid-feedback" style="width: 100%;">
+			***
+		</div>
+	</div>
+</div>
+
+<div class="mb-3">
+	<label for="username">Crown Pixel</label>
+	<div class="input-group">
+		<input type="text" class="form-control" id="username" placeholder="***">
+		<div class="input-group-prepend">
+			<span class="input-group-text">Crown Pixel</span>
+		</div>
+		<div class="invalid-feedback" style="width: 100%;">
+			***
+		</div>
+	</div>
+</div>
+
+<div class="mb-3">
+	<label for="username">Canopy200</label>
+	<div class="input-group">
+		<input type="text" class="form-control" id="username" placeholder="***">
+		<div class="input-group-prepend">
+			<span class="input-group-text">Canopy200</span>
+		</div>
+		<div class="invalid-feedback" style="width: 100%;">
+			***
+		</div>
+	</div>
+</div>
+
+<div class="mb-3">
+	<label for="username">Canopy800</label>
+	<div class="input-group">
+		<input type="text" class="form-control" id="username" placeholder="***">
+		<div class="input-group-prepend">
+			<span class="input-group-text">Canopy800</span>
+		</div>
+		<div class="invalid-feedback" style="width: 100%;">
+			***
+		</div>
+	</div>
+</div>
+
+<hr class="mb-4">
+<!-- button class="btn btn-primary btn-lg btn-block" type="submit">Continue to checkout</button -->
+</form>
+<!-- //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+	</div>
+	<div class="col-md-6">
+		<div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+			<div class="col p-4 d-flex flex-column position-static">
+				<h2>Techniques:</h2>
+				<form>
+				<div class="form-group">
+					<div class="input-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text">1. Modelling</span>
+						</div>
+						<div class="invalid-feedback" style="width: 100%;">
+							***
+						</div>
+					</div>
+				</div><!-- /  class="form-group" -->
+				<div class="input-group">
+					<div class="input-group-prepend">
+						<span class="input-group-text">2. Machine learning </span>
+					</div>
+					<select class="form-control" id="exampleFormControlSelect2">
+						<option>Frond Number</option>
+						<option>Frond Angle</option>
+						<option>Crown Pixel</option>
+						<option>Canopy200</option>
+						<option>Canopy800</option>
+					</select>
+					<div class="invalid-feedback" style="width: 100%;">
+						***
+					</div>
+				</div><!-- /  class="form-group" -->
+				<hr class="mb-4"><br><br><br><br><br>
+				<div class="input-group">
+					<div class="input-group-prepend">
+						<span class="input-group-text">Healthiness levels </span>
+					</div>
+					<input type="checkbox" class="form-control" id="same-address">
+					<div class="invalid-feedback" style="width: 100%;">
+						***
+					</div>
+				</div><!-- /  class="form-group" -->
+				</form>
+			</div>
+		</div>
+	</div>
+	</div>
+</div><!-- ./ class="container" -->
+
+
+<!-- FOOTER -->
+<footer class="footer mt-auto py-3">
+<div class="containerFooter">
+	<p class="float-right"><a href="#">Back to top</a></p>
+	<p>&copy; 2017-2019 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+</div><!-- ./ class="container" -->
+</footer>
+
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////// -->
+<!-- script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script -->
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<?php include '../dibawah.php';
+?>
+</body>
+</html>
---
+++ sumber/fail/js/highchart/tarikdatacsv/9-oil-palm-monitoring.php
@@ -0,0 +1,68 @@
+<?php
+//include '../diatas.php';
+include '../diatas001.php';
+?>
+
+<!-- x. Add the container -->
+<div class="container">
+	<h1>Oil Palm Monitoring</h1>
+	<div class="row mb-2">
+	<div class="col-md-6">
+<!-- //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+		<div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+			<div class="col p-4 d-flex flex-column position-static">
+				<h2>Techniques:</h2>
+				<form>
+				<div class="form-group">
+					<div class="input-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text">Tree ID:</span>
+						</div>
+						<input type="text" class="form-control" id="username" placeholder="***">
+						<div class="invalid-feedback" style="width: 100%;">
+							***
+						</div>
+					</div>
+				</div><!-- /  class="form-group" -->
+				<div class="input-group">
+					<div class="input-group-prepend">
+						<span class="input-group-text">Coordinates:</span>
+					</div>
+					<input type="text" class="form-control" id="username" placeholder="***">
+					<div class="invalid-feedback" style="width: 100%;">
+						***
+					</div>
+				</div><!-- /  class="form-group" -->
+				<hr class="mb-4"><br>
+				<div class="input-group">
+					<div class="input-group-prepend">
+						<span class="input-group-text">ArcGIS Online</span>
+					</div>
+					<div class="invalid-feedback" style="width: 100%;">
+						***
+					</div>
+				</div><!-- /  class="form-group" -->
+				</form>
+			</div>
+		</div>
+<!-- //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+	</div>
+	</div>
+</div><!-- ./ class="container" -->
+
+
+<!-- FOOTER -->
+<footer class="footer mt-auto py-3">
+<div class="containerFooter">
+	<p class="float-right"><a href="#">Back to top</a></p>
+	<p>&copy; 2017-2019 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+</div><!-- ./ class="container" -->
+</footer>
+
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////// -->
+<!-- script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script -->
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<?php include '../dibawah.php';
+?>
+</body>
+</html>
