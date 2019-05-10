fe8f392177aa329cb93cf57f585223a6e46fae44
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-05-10 17:41:53 +0800

    ganti data baru

--- sumber/fail/js/highchart/latihan002.php
+++ sumber/fail/js/highchart/latihan002.php
@@ -3,49 +3,77 @@
 <div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
 
 <table id="datatable">
-<thead><tr><th>Tree_ID</th><th>Fronds_no</th></tr></thead>
+<thead><tr><th></th><th>Tree ID</th></tr></thead>
 <tbody>
-	<tr><td>74</td><td>21</td></tr>
-	<tr><td>83</td><td>23</td></tr>
-	<tr><td>84</td><td>28</td></tr>
-	<tr><td>136</td><td>28</td></tr>
-	<tr><td>312</td><td>23</td></tr>
-	<tr><td>313</td><td>24</td></tr>
-	<tr><td>314</td><td>22</td></tr>
-	<tr><td>319</td><td>20</td></tr>
-	<tr><td>320</td><td>19</td></tr>
-	<tr><td>321</td><td>22</td></tr>
-	<tr><td>62</td><td>20</td></tr>
-	<tr><td>73</td><td>21</td></tr>
-	<tr><td>81</td><td>16</td></tr>
-	<tr><td>82</td><td>22</td></tr>
-	<tr><td>95</td><td>16</td></tr>
-	<tr><td>112</td><td>19</td></tr>
-	<tr><td>122</td><td>20</td></tr>
-	<tr><td>1213</td><td>20</td></tr>
-	<tr><td>1214</td><td>20</td></tr>
-	<tr><td>1218</td><td>21</td></tr>
-	<tr><td>126</td><td>12</td></tr>
-	<tr><td>134</td><td>13</td></tr>
-	<tr><td>143</td><td>13</td></tr>
-	<tr><td>145</td><td>12</td></tr>
-	<tr><td>149</td><td>15</td></tr>
-	<tr><td>219</td><td>16</td></tr>
-	<tr><td>612</td><td>12</td></tr>
-	<tr><td>821</td><td>10</td></tr>
-	<tr><td>1018</td><td>15</td></tr>
-	<tr><td>1416</td><td>11</td></tr>
-	<tr><td>43</td><td>9</td></tr>
-	<tr><td>44</td><td>8</td></tr>
-	<tr><td>72</td><td>8</td></tr>
-	<tr><td>135</td><td>10</td></tr>
-	<tr><td>138</td><td>14</td></tr>
-	<tr><td>144</td><td>9</td></tr>
-	<tr><td>413</td><td>8</td></tr>
-	<tr><td>1310</td><td>12</td></tr>
-	<tr><td>1412</td><td>12</td></tr>
-	<tr><td>1415</td><td>15</td></tr>
+	<tr><th>ID 74</th><td>21</td></tr>
+	<tr><th>ID 83</th><td>23</td></tr>
+	<tr><th>ID 84</th><td>28</td></tr>
+	<tr><th>ID 136</th><td>28</td></tr>
+	<tr><th>ID 312</th><td>23</td></tr>
+	<tr><th>ID 313</th><td>24</td></tr>
+	<tr><th>ID 314</th><td>22</td></tr>
+	<tr><th>ID 319</th><td>20</td></tr>
+	<tr><th>ID 320</th><td>19</td></tr>
+	<tr><th>ID 321</th><td>22</td></tr>
+	<tr><th>ID 62</th><td>20</td></tr>
+	<tr><th>ID 73</th><td>21</td></tr>
+	<tr><th>ID 81</th><td>16</td></tr>
+	<tr><th>ID 82</th><td>22</td></tr>
+	<tr><th>ID 95</th><td>16</td></tr>
+	<tr><th>ID 112</th><td>19</td></tr>
+	<tr><th>ID 122</th><td>20</td></tr>
+	<tr><th>ID 1213</th><td>20</td></tr>
+	<tr><th>ID 1214</th><td>20</td></tr>
+	<tr><th>ID 1218</th><td>21</td></tr>
+	<tr><th>ID 126</th><td>12</td></tr>
+	<tr><th>ID 134</th><td>13</td></tr>
+	<tr><th>ID 143</th><td>13</td></tr>
+	<tr><th>ID 145</th><td>12</td></tr>
+	<tr><th>ID 149</th><td>15</td></tr>
+	<tr><th>ID 219</th><td>16</td></tr>
+	<tr><th>ID 612</th><td>12</td></tr>
+	<tr><th>ID 821</th><td>10</td></tr>
+	<tr><th>ID 1018</th><td>15</td></tr>
+	<tr><th>ID 1416</th><td>11</td></tr>
+	<tr><th>ID 43</th><td>9</td></tr>
+	<tr><th>ID 44</th><td>8</td></tr>
+	<tr><th>ID 72</th><td>8</td></tr>
+	<tr><th>ID 135</th><td>10</td></tr>
+	<tr><th>ID 138</th><td>14</td></tr>
+	<tr><th>ID 144</th><td>9</td></tr>
+	<tr><th>ID 413</th><td>8</td></tr>
+	<tr><th>ID 1310</th><td>12</td></tr>
+	<tr><th>ID 1412</th><td>12</td></tr>
+	<tr><th>ID 1415</th><td>15</td></tr>
 </tbody>
 </table>
 
-<?php include 'dibawah2.php'; ?>
+<?php include 'dibawah.php'; ?>
+<script type="text/javascript">
+	Highcharts.chart('container', {
+	data: { table: 'datatable' },
+	chart: { type: 'column' },
+	title: { text: 'Data tarik dari HTML table dalam halaman ini'  },
+	yAxis: {
+		allowDecimals: false,
+		title: { text: 'Fronds_no' }
+	},
+	plotOptions: {
+		series: {
+			borderWidth: 0,
+			dataLabels: {
+				enabled: true,
+				format: '{point.y}'
+			}
+		}
+	},
+	tooltip: {
+		formatter: function () {
+			return '<b>' + this.series.name + '</b><br/>' +
+			this.point.y + ' ' + this.point.name.toLowerCase();
+		}
+	}
+});
+</script>
+</body>
+</html>
