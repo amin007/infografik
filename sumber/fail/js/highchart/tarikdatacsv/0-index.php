9639f04b748cd287c9c12e0286c461050296a88b
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-05-11 01:08:58 +0800

    menambah maklumat tambahan

--- sumber/fail/js/highchart/tarikdatacsv/0-index.php
+++ sumber/fail/js/highchart/tarikdatacsv/0-index.php
@@ -5,7 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="description" content="">
 <meta name="author" content="Amin Ledang">
-<title>3-first-scan-frond-number.php</title>
+<title>Main System</title>
 <!-- Bootstrap core CSS -->
 <link href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" rel="stylesheet">
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
@@ -13,7 +13,8 @@
 <body>
 <!-- //////////////////////////////////////////////////////////////////////////////////// -->
 <br>
-<div class ="container">
+<div class="container">
+<h1>BSR Detection and Monitoring System</h1>
 <div class="bd-example bd-example-tabs">
 	<ul class="nav nav-tabs" id="myTab" role="tablist">
 	<li class="nav-item">
@@ -44,13 +45,63 @@
 		<p>Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings gentrify squid 8-bit cred pitchfork. Williamsburg banh mi whatever gluten-free, carles pitchfork biodiesel fixie etsy retro mlkshk vice blog. Scenester cred you probably haven't heard of them, vinyl craft beer blog stumptown. Pitchfork sustainable tofu synth chambray yr.</p>
 	</div>
 	<div class="tab-pane fade" id="login" role="tabpanel" >
-		<a class="dropdown-item" href="9-oil-palm-monitoring.php">Dashboard</a>
+	<!-- @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+		<div class="row mb-2">
+		<div class="col-md-6">
+			<div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+				<div class="col p-4 d-flex flex-column position-static">
+					<h3 class="mb-0">Username:</h3>
+					<h3 class="mb-0">Password:</h3>
+					<a class="dropdown-item" href="9-oil-palm-monitoring.php">Dashboard</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+				<div class="col p-4 d-flex flex-column position-static">
+					<div class="mb-1 text-muted">Field infomation:</div>
+					<div class="mb-1 text-muted">Field name:</div>
+					<div class="mb-1 text-muted">No. of palms:</div>
+					<div class="mb-1 text-muted">Area/Location:</div>
+					<div class="mb-1 text-muted">Manager's Name:</div>
+					<div class="mb-1 text-muted">Size of plantation:</div>
+				</div>
+			</div>
+		</div>
+		</div>
+	<!-- @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ -->
 	</div>
 	<!-- ############################################################################################################################################### -->
 	</div>
 </div>
 </div><!-- / class ="container" -->
-
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////// -->
+<!-- Marketing messaging and featurettes
+================================================== -->
+<!-- Wrap the rest of the page in another container to center all the content. -->
+<hr><div class="container">
+<!-- Three columns of text below the carousel -->
+<div class="row">
+	<div class="col-lg-4">
+		<svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+		<h2>Soil Sensor Network System</h2>
+		<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+		<p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+	</div><!-- /.col-lg-4 -->
+	<div class="col-lg-4">
+		<svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+		<h2>Laser Scanning Canopy Biometrics</h2>
+		<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+		<p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+	</div><!-- /.col-lg-4 -->
+	<div class="col-lg-4">
+		<svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+		<h2>Spectral Properties</h2>
+		<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+		<p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+	</div><!-- /.col-lg-4 -->
+</div><!-- /.row -->
+</div><!-- /.class="container" -->
 <!-- ////////////////////////////////////////////////////////////////////////////////////////////// -->
 <!-- script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script -->
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
